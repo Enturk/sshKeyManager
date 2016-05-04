442c4c4471f87b9b6c26ef6b592a0a6e86846f5f
Enturk <nazimkaraca@gmail.com>
2016-05-04 15:15:53 -0400

    Update hostListMaker.py

--- hostListMaker.py
+++ hostListMaker.py
@@ -1,57 +1,49 @@
-# Nazim Karaca made this, with Will Weber's help, for University of Delaware in 2016.
-#!/usr/bin/env python2
+#!/bin/bash 
+LOGFILE=errorlog # change to /dev/null if unwanted
+date +"%Y_%m_%d" >> $LOGFILE
 
-from sys import argv
-import os
-import socket
-import re
-hostfilename = argv[1] #alternative filenaming mechanism, better because passed by prior script
 
-NewHostList = open(hostfilename, "a")
-hostspath = "./hosts/"
-keyspath = "./keys/"
-verbose = True
-
-if verbose == False:
-    errorlog = open("errorlog", "a")
+# get hosts from internal file, and then ssh-keyscan each one of them
+cat keys/sample.txt | while read line; do 
+    hostName=`echo $line | awk -F' ' '{print $1}' | awk -F',' '{print $2}'` #will give all host names in the key file
+    # echo "Checking $hostName"
+    keyInfo=`ssh-keyscan $hostName 2>>$LOGFILE | awk -F' ' '{print $2,$3}'` #will grab all key types and public keys
     
-# append all the correct codes 
-hostlist = os.listdir(hostspath)
-for file in hostlist:
-    try:
+    #check if hostname file already esists, if not, create it 
+    [ -r "hosts/$hostName" ] || touch "hosts/$hostName"
+    
+    # compare hostName and keyInfo to existing file, create .changed if different.
+    if [ "$(< hosts/$hostName)" != "$keyInfo" -a -n "$keyInfo" ] ; then # 0 if true
+        # write changes to a new file
+        echo -n "$keyInfo" > hosts/${hostName}.changed
+        # maybe TODO log actions?
         
-        # get network feedback
-        ip = socket.gethostbyname(file)
-        content = open(hostspath+file,"r").read()
-        content.replace(" ",",")
+        echo $hostName is different from our info. | tee -a $LOGFILE
+    else
+        #overwrite old info to file
+        echo "Keeping old key file for $hostName because no response from network."
+        echo $line | awk -F',' '{print $NF}' | awk -F' ' '{print $2,$3}' > hosts/${hostName}
+    fi
+done
+
+if [ \! -e hosts/*.changed ] ; then
+    echo 'No changes detected, doc. List NOT updated.'
+else
+    echo 'Hey, YOU! Commit dem changes? [Y/N]'
+    read answer
+    if [ $answer =  'Y' -o $answer = 'y' ] ; then
+        for filename in hosts/*.changed 
+        do
+            mv ${filename} ${filename%%.changed} # why does this look at t5120-hw-etc...?
+        done
         
-        # get host info including aliases
-        host = ""
-        OldHostList = open(argv[2], "r")
-        for line in OldHostList:
-            if file in line:
-                print "found print in old host list"
-                host = line[0:line.find(" ")]
-        
-        # write the info
-        NewHostList.write(host+" "+content+"\n")
-        if verbose: print "Updated: "+file
-        else: errorlog.write("Updated: "+file+" in "+hostfilename)
-        
-        OldHostList.close()
-        
-    except socket.gaierror:
-        
-        # keep old host info
-        OldHostList = open(argv[2], "r")
-        for line in OldHostList:
-            if file in line:
-                NewHostList.write(line)
-                if verbose: print file+" not found on the network, keeping old info in updated list."
-                else: errorlog.write(file+" not found on the network, keeping old info in updated list.")
-        
-        OldHostList.close()
-        
-NewHostList.close()
-if verbose == False:
-    errorlog.close()
+        #TODO log actions
+        mv keys/updated_keys* keys/oldKeylists/ # move old key file
+        oldlist=$(ls -dt keys/oldKeylists/* | head -1)
+        newlist=keys/updated_keys_`date +"%Y_%m_%d"`
+        touch $newlist
+
+        # run python script to dump info into that file
+        exec python hostListMaker.py $newlist $oldlist
+    fi
+fi
