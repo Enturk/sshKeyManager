bca133568196d730b6e60daa378a9126e926b34f
Enturk <nazimkaraca@gmail.com>
2016-05-04 15:02:57 -0400

    Update hostListMaker.py
    Added aliases, fixed spacing after hostname

--- hostListMaker.py
+++ hostListMaker.py
@@ -10,27 +10,49 @@ hostfilename = argv[1] #alternative filenaming mechanism, better because passed 
 NewHostList = open(hostfilename, "a")
 hostspath = "./hosts/"
 keyspath = "./keys/"
+verbose = True
 
-# errorlog = open("errorlog", "a")
-
+if verbose == False:
+    errorlog = open("errorlog", "a")
+    
 # append all the correct codes 
 hostlist = os.listdir(hostspath)
 for file in hostlist:
     try:
+        
+        # get network feedback
         ip = socket.gethostbyname(file)
         content = open(hostspath+file,"r").read()
         content.replace(" ",",")
-        NewHostList.write(ip+","+file+","+content+"\n")
-        print "Updated: "+file
+        
+        # get host info including aliases
+        host = ""
+        OldHostList = open(argv[2], "r")
+        for line in OldHostList:
+            if file in line:
+                print "found print in old host list"
+                host = line[0:line.find(" ")]
+        
+        # write the info
+        print host
+        NewHostList.write(host+" "+content+"\n")
+        if verbose: print "Updated: "+file
+        else: errorlog.write("Updated: "+file+" in "+hostfilename)
+        
+        OldHostList.close()
+        
     except socket.gaierror:
+        
         # keep old host info
         OldHostList = open(argv[2], "r")
         for line in OldHostList:
             if file in line:
-                print file+" not found on the network, keeping old info in updated list."
                 NewHostList.write(line)
+                if verbose: print file+" not found on the network, keeping old info in updated list."
+                else: errorlog.write(file+" not found on the network, keeping old info in updated list.")
+        
         OldHostList.close()
         
-# if argv[2] != "": print "Here's your file %r:" % NewHostList
-
 NewHostList.close()
+if verbose == False:
+    errorlog.close()
